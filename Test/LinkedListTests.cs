9717b4b645b9595ffec5b1c747ce6383a337d099
Dung Nguyen <Dung.Nguyen@hansencx.com>
2019-09-26 17:40:14 +0700

    update

--- Test/LinkedListTests.cs
+++ Test/LinkedListTests.cs
@@ -10,35 +10,87 @@ namespace Test
         [Test]
         public void Create_SetValue_And_Next_Is_Null()
         {
-            ListNode<int> node = new ListNode<int>();
-            node.Set(1);
-            node.Get().Should().Be(1);
-            node.Next().Should().Be(null);
+            ListNode<int> node = new ListNode<int>(1);
+            node.Data.Should().Be(1);
+            node.Next.Should().Be(null);
+        }
+
+        [Test]
+        public void Can_Add_First()
+        {
+            var myLinkedList = new MyLinkedList<int>();
+            myLinkedList.AddFirst(new ListNode<int>(1));
+            myLinkedList.GetHead().Data.Should().Be(1);
+        }
+
+        [Test]
+        public void Can_Add_Last()
+        {
+            var myLinkedList = new MyLinkedList<int>();
+            myLinkedList.AddLast(new ListNode<int>(1));
+            myLinkedList.GetLast().Data.Should().Be(1);
+            myLinkedList.GetLast().Next.Should().Be(null);
         }
     }
 
     public class ListNode<T>
     {
-        private T _data;
-        private ListNode<T> _next;
+        public T Data;
+        public ListNode<T> Next;
 
-        public void Set(T data)
+        public ListNode(T data, ListNode<T> next = null)
         {
-            _data = data;
-        }
-
-        public T Get()
-        {
-            return _data;
-        }
-
-        public ListNode<T> Next()
-        {
-            return _next;
+            Data = data;
+            Next = next;
         }
     }
 
-    public class MyLinkedList
+    public class MyLinkedList<T>
     {
+        private ListNode<T> _head;
+
+        public void AddFirst(ListNode<T> listNode)
+        {
+            listNode.Next = _head;
+            _head = listNode;
+        }
+
+        public ListNode<T> GetHead()
+        {
+            return _head;
+        }
+
+        public void AddLast(ListNode<T> listNode)
+        {
+            var p = _head;
+            if (p == null)
+            {
+                _head = listNode;
+                return;
+            }
+
+            while (p.Next != null)
+            {
+                p = p.Next;
+            }
+
+            p.Next = listNode;
+        }
+
+        public ListNode<T> GetLast()
+        {
+            var p = _head;
+            if (p == null)
+            {
+                return null;
+            }
+
+            while (p.Next != null)
+            {
+                p = p.Next;
+            }
+
+            return p;
+        }
     }
 }
